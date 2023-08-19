b589c597b81df0bc5b5a55bfd935de07901dc676
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-08-19 19:56:58 +1000

    Create home screen placeholder.

--- App.js
+++ App.js
@@ -1,12 +1,59 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView, StyleSheet } from "react-native";
 
 const App = () => {
   return (
-    <View>
-      <Text>Current Weather</Text>
-    </View>
+    <SafeAreaView style={styles.wrapper}>
+      <View style={styles.container}>
+        <Text style={styles.temp}>6</Text>
+        <Text style={styles.feels}>feels like 5</Text>
+        <View style={styles.highLowWrapper}>
+          <Text style={styles.highLow}>Heigh: 8</Text>
+          <Text style={styles.highLow}> Low: 6</Text>
+        </View>
+      </View>
+      <View style={styles.bodyWrapper}>
+        <Text style={styles.description}>Its sunny</Text>
+        <Text style={styles.message}>Its perfect t-shirt weather</Text>
+      </View>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    backgroundColor: "pink",
+  },
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  temp: {
+    color: "black",
+    fontSize: 48,
+  },
+  feels: {
+    color: "black",
+    fontSize: 30,
+  },
+  highLowWrapper: {
+    flexDirection: "row",
+  },
+  highLow: {
+    color: "black",
+    fontSize: 20,
+  },
+  bodyWrapper: {
+    justifyContent: "flex-end",
+    alignItems: "flex-start",
+  },
+  description: {
+    fontSize: 48,
+  },
+  message: {
+    fontSize: 30,
+  },
+});
 
 export default App;
