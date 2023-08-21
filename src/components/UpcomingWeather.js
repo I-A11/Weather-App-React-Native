65f6af03c1890733b0bd427695289a915ac5e47f
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-08-21 10:07:43 +1000

    Start creating weather list in upcoming weather component.

--- src/components/UpcomingWeather.js
+++ src/components/UpcomingWeather.js
@@ -1,10 +1,66 @@
 import React from "react";
-import { Text, StyleSheet, SafeAreaView } from "react-native";
+import { Text, StyleSheet, SafeAreaView, FlatList, View } from "react-native";
+import { Feather } from "@expo/vector-icons";
+
+const DATA = [
+  {
+    dt_txt: "2023-02-18 12:00:00",
+    main: {
+      temp_max: 8.55,
+      temp_min: 7.55,
+    },
+    weather: [
+      {
+        main: "Clear",
+      },
+    ],
+  },
+  {
+    dt_txt: "2023-02-18 12:00:00",
+    main: {
+      temp_max: 8.55,
+      temp_min: 7.55,
+    },
+    weather: [
+      {
+        main: "Clouds",
+      },
+    ],
+  },
+  {
+    dt_txt: "2023-02-18 18:00:00",
+    main: {
+      temp_max: 8.55,
+      temp_min: 7.55,
+    },
+    weather: [
+      {
+        main: "Rain",
+      },
+    ],
+  },
+];
+
+const Item = (props) => {
+  const { dt_txt, min, max, condition } = props;
+  return (
+    <View>
+      <Feather name={"sun"} size={50} color={"white"} />
+      <Text>{dt_txt}</Text>
+      <Text>{min}</Text>
+      <Text>{max}</Text>
+    </View>
+  );
+};
 
 const UpcomingWeather = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Text>Upcoming Weather</Text>
+      <FlatList
+        data={DATA}
+        // renderItem={}
+      />
     </SafeAreaView>
   );
 };
