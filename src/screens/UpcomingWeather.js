ee05b5566429f4be5977b0b6389aef98600e539d
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-08-22 09:48:59 +1000

    Make screens folder to keep all app pages and refactor the code structure.

--- src/screens/UpcomingWeather.js
+++ src/screens/UpcomingWeather.js
@@ -4,10 +4,10 @@ import {
   StyleSheet,
   SafeAreaView,
   FlatList,
-  View,
   StatusBar,
+  ImageBackground,
 } from "react-native";
-import { Feather } from "@expo/vector-icons";
+import ListItem from "../components/ListItem";
 
 const DATA = [
   {
@@ -48,36 +48,29 @@ const DATA = [
   },
 ];
 
-const Item = (props) => {
-  const { dt_txt, min, max, condition } = props;
-  return (
-    <View style={styles.item}>
-      <Feather name={"sun"} size={50} color={"white"} />
-      <Text style={styles.date}>{dt_txt}</Text>
-      <Text style={styles.temp}>{min}</Text>
-      <Text style={styles.temp}>{max}</Text>
-    </View>
-  );
-};
-
 const UpcomingWeather = () => {
   const renderItem = ({ item }) => (
-    <Item
+    <ListItem
       condition={item.weather.main}
       dt_txt={item.dt_txt}
       min={item.main.temp_min}
       max={item.main.temp_max}
     />
   );
-
+  const { container, image } = styles;
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Upcoming Weather</Text>
-      <FlatList
-        data={DATA}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.dt_txt}
-      />
+    <SafeAreaView style={container}>
+      <ImageBackground
+        source={require("../../assets/upcoming-weather.jpg")}
+        style={image}
+      >
+        <Text>Upcoming Weather</Text>
+        <FlatList
+          data={DATA}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.dt_txt}
+        />
+      </ImageBackground>
     </SafeAreaView>
   );
 };
@@ -86,7 +79,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: StatusBar.currentHeight || 0,
-    backgroundColor: "red",
+    backgroundColor: "royalblue",
   },
   item: {
     padding: 20,
@@ -105,6 +98,9 @@ const styles = StyleSheet.create({
   date: {
     color: "white",
     fontSize: 15,
+  },
+  image: {
+    flex: 1,
   },
 });
 export default UpcomingWeather;
