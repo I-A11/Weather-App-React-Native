69ae3b261e7736af6de43b5f7abbdd4dd93e44ad
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-08-23 19:50:45 +1000

    Create fetch function to fetch data from open weather api.

--- App.js
+++ App.js
@@ -1,13 +1,67 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { ActivityIndicator, View, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./src/components/Tabs";
+import * as Location from "expo-location";
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+const weatherKey = process.env.EXPO_PUBLIC_WEATHER_API_KEY;
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [weather, setWeather] = useState([]);
+  const [lat, setLat] = useState([]);
+  const [lon, setLon] = useState([]);
+
+  const fetchWeatherData = async () => {
+    try {
+      const res = await fetch(
+        `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weatherKey}`
+      );
+      const data = await res.json();
+      setWeather(data);
+    } catch (error) {
+      setError("Could not fetch weather");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setError("Permission to access location was denied");
+        return;
+      }
+      let location = await Location.getCurrentPositionAsync({});
+      setLat(location.coords.latitude);
+      setLon(location.coords.longitude);
+      await fetchWeatherData();
+    })();
+  }, [lat, lon]);
+
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size={"large"} color={"blue"} />
+      </View>
+    );
+  }
+
   return (
     <NavigationContainer>
       <Tabs />
     </NavigationContainer>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+});
 
 export default App;
